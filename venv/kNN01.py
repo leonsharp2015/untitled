ccbd2a3c7a2ef6159e0dd277deea1a8ec0de8719
leonsharp2015 <leonsharp2015@sina.com>
2019-12-24 01:10:29 +0800

    knn

--- venv/kNN01.py
+++ venv/kNN01.py
@@ -1,5 +1,7 @@
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 def createDataSet():
     group=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels=['A','A','B','B']
@@ -19,10 +21,52 @@ def classify01(inX,dataSet,labels,k):
     sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
 
+def file_ndArray():
+    fr=open('/Users/zhanglei/机器学习与算法/机器学习实战源代码/machinelearninginaction/Ch02/datingTestSet2.txt')
+    arrayLines=fr.readlines()
+    numLines=len(arrayLines)
+    returnMat=zeros((numLines,3))#ndarray
+    index=0
+    classLabelVector=[]
+    for line in arrayLines:
+        line=line.strip() #移除字符串头尾指定的字符,默认为空格或换行符,或字符序列
+        listFromline=line.split('\t')
+        returnMat[index,:]=listFromline[0:3]
+        index+=1
+        classLabelVector.append(int(listFromline[-1]))
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minValue=dataSet.min(0)
+    print(minValue)
+    maxValue=dataSet.max(0)
+    ranges=maxValue-minValue
+    normalSet=zeros(shape(dataSet))
+    m=dataSet.shape(0)
+    normalDataSet=dataSet-tile(minValue,(m,1))
+    normalDataSet=normalDataSet/tile(ranges,(m,1))
 
 
-g,lbl=createDataSet()
-c1=classify01([0,0],g,lbl,3)
-v1=[1,2]
-# print(tile(v1,(2,2)))
-# print(c1)
+# g,lbl=createDataSet()
+# c1=classify01([0,0],g,lbl,3)
+
+dataMatrix,datalabels=file_ndArray()
+# fig=plt.figure()
+# ax=fig.add_subplot(111)
+# ax.scatter(dataMatrix[:,1],dataMatrix[:,2],15.0*array(datalabels),15.0*array(datalabels))
+# ax.axis([-2,25,-0.2,2.0])
+# plt.show()
+ndarray=array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+data=zeros(shape(ndarray))
+#data=ndarray-tile()
+minValues=ndarray.min(0)
+maxValues=ndarray.max(0)
+ranges=maxValues-minValues
+normalSet=ndarray-tile(minValues,(4,1))
+normalSet=normalSet/tile(ranges,(4,1))
+print(ranges)
+
+
+
+
+

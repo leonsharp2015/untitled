dd91d8758dbc84e6f220c5f703436b5f2f3cd23b
leonsharp2015 <leonsharp2015@sina.com>
2020-01-21 11:56:49 +0800

    kmeans

--- venv/kmeans/kmean1.py
+++ venv/kmeans/kmean1.py
@@ -57,22 +57,61 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
     return centroids, clusterAssment
 
 
-dataMat=loadDataSet('/Users/zhanglei/机器学习与算法/机器学习实战源代码/machinelearninginaction/Ch10/testSet.txt')
+#二分K-均值聚类
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))
+    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centList =[centroid0] #create a list with one centroid
+    for j in range(m):#calc initial Error
+        clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2
+    while (len(centList) < k):
+        lowestSSE = inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]#get the data points currently in cluster i
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = sum(splitClustAss[:,1])#compare the SSE to the currrent minimum
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
+            print("sseSplit, and notSplit: ",sseSplit,sseNotSplit)
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList) #change 1 to 3,4, or whatever
+        bestClustAss[nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit
+        print('the bestCentToSplit is: ',bestCentToSplit)
+        print('the len of bestClustAss is: ', len(bestClustAss))
+        centList[bestCentToSplit] = bestNewCents[0,:].tolist()[0]#replace a centroid with two best centroids
+        centList.append(bestNewCents[1,:].tolist()[0])
+        clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:]= bestClustAss#reassign new clusters, and SSE
+    return mat(centList), clusterAssment
+
+
+
+# dataMat=loadDataSet('/Users/zhanglei/机器学习与算法/机器学习实战源代码/machinelearninginaction/Ch10/testSet.txt')
 # init_centroids=randCent(dataMat,4) #4*2的簇
-centroids, clusterAssment=kMeans(dataMat,4) #4*2
+# centroids, clusterAssment=kMeans(dataMat,4) #4*2
 # print(centroids)
 
+
+dataMat=loadDataSet('/Users/zhanglei/机器学习与算法/机器学习实战源代码/machinelearninginaction/Ch10/testSet2.txt')
+centList,clusterAssment=biKmeans(dataMat,3)
+print(centList)
+
+
+#对nozero的测试
 # a=array([[1,0,0],[0,21,0],[3,7,9]])#[0,1,2,2,2],[0,1,0,1,2]:3,7,9都非0，所以第一个array补齐2
 # # a = array([[1, 0], [2, 0], [0, 9]])#不为0元素的行索引，放在第一个array[0,1,2]，列索引，放在第二个array[0,0,1]
 # b = nonzero(a)
 # print(b)
 
 
-array1=array([[1,2,9],[1,6,7],[2,5,6]])
-mat1=mat(array1)
-b1=nonzero(mat1[:,0]==1)#nonzero([[True],[True],[False]])=>存在的行索引array(0,1),存在的列索引array(0,0)
-row=b1[0]
-print(mat1[row])
+# array1=array([[1,2,9],[1,6,7],[2,5,6]])
+# mat1=mat(array1)
+# b1=nonzero(mat1[:,0]==1)#nonzero([[True],[True],[False]])=>存在的行索引array(0,1),存在的列索引array(0,0)
+# row=b1[0]
+# print(mat1[row])
 
 
 

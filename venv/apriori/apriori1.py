489e4bb4851317a43dbe94d8693d8aea49603e9b
leonsharp2015 <leonsharp2015@sina.com>
2020-01-23 12:12:28 +0800

    aprior

--- venv/apriori/apriori1.py
+++ venv/apriori/apriori1.py
@@ -15,19 +15,17 @@ def createC1(dataSet):
     # 将C1冰冻，即固定列表C1，使其不可变
     return map(frozenset, C1)  # use frozen set so we can use it as a key in a dict
 
-def scanD(D, Ck, minSupport):#D, Ck都是迭代器
-    D_copy = copy.deepcopy(D)
-    can_list=list(Ck)
+def scanD(D, Ck, minSupport):
     ssCnt = {}
     for tid in list(D):
-        for can in can_list:
+        for can in Ck:
             if can.issubset(tid):
                 if not can in ssCnt:
                     ssCnt[can] = 1
                 else:
                     ssCnt[can] += 1
 
-    numItems = len(list(D_copy))
+    numItems = len(D)
     retList = []
     supportData = {}
     for key in ssCnt:
@@ -38,7 +36,7 @@ def scanD(D, Ck, minSupport):#D, Ck都是迭代器
     return retList, supportData
 
 #------------------------
-def aprioriGen(Lk, k): #creates Ck
+def aprioriGen(Lk, k): #根据元素，构造笛卡尔项集,k是集合的元素个数2,3
     retList = []
     lenLk = len(Lk)
     for i in range(lenLk):
@@ -50,32 +48,33 @@ def aprioriGen(Lk, k): #creates Ck
     return retList
 
 def apriori(dataSet, minSupport = 0.5):
-    C1 = createC1(dataSet)
-    D = map(set, dataSet)#遍历器
-    D2=map(set, dataSet)
-    L1, supportData = scanD(D, C1, minSupport)
+    C1 = createC1(dataSet)#集合的所有元素
+    D = map(set, dataSet)#D集合，是遍历器
+    C1=list(C1)
+    D=list(D)
+    L1, supportData = scanD(D, C1, minSupport)#得到每个元素在集合中存在的次数，返回字典
     L = [L1]
     k = 2
     while (len(L[k-2]) > 0):
-        Ck = aprioriGen(L[k-2], k)
-        Lk, supK = scanD(D2, Ck, minSupport)#scan DB to get Lk
+        Ck = aprioriGen(L[k-2], k)#按个数k，将每个元素笛卡尔，构成项集cK
+        Lk, supK = scanD(D, Ck, minSupport)#项集cK在集合中出现的次数，返回字典
         supportData.update(supK)
         L.append(Lk)
         k += 1
     return L, supportData
 
 
-
-
 # ds=loadDataSet()
 # D=map(set,ds) #python3 map(set,ds)是迭代器.[[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
 # c1=createC1(ds) #[frozenset({1}), frozenset({2}), frozenset({3}), frozenset({4}), frozenset({5})]
 # #L1:每个单物品项集至少出现在50%以上的记录中
+# D=list(D)
+# c1=list(c1)
 # L1,suppd=scanD(D,c1,0.75)#[frozenset({1}), frozenset({3}), frozenset({2}), frozenset({5})]
 # print(L1)
 
 ds=loadDataSet()
 L,suppData=apriori(ds)
-print(L[2])
+print(L)
 
 

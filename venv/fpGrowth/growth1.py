aa9cdfc2dc798e3ce6ec30b2fd74f5dcc46a13c1
leonsharp2015 <leonsharp2015@sina.com>
2020-01-26 18:07:34 +0800

    fpgrowth

--- venv/fpGrowth/growth1.py
+++ venv/fpGrowth/growth1.py
@@ -24,37 +24,43 @@ def createTree(dataSet, minSup=1):  # create FP-tree from dataset but don't mine
         for item in trans:
             headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
     for k in list(headerTable.keys()):  # remove items not meeting minSup
-        if headerTable[k] < minSup:
+        if headerTable[k] < minSup:#headerTable保存每个单词出现的次数
             del (headerTable[k])
-    freqItemSet = set(headerTable.keys())
+    freqItemSet = set(headerTable.keys())#freqItemSet是出现次数大于阈值的单词集合
     # print 'freqItemSet: ',freqItemSet
     if len(freqItemSet) == 0: return None, None  # if no items meet min support -->get out
     for k in headerTable:
-        headerTable[k] = [headerTable[k], None]  # reformat headerTable to use Node link
+        #headerTable是dictinary类型：key=单词，value＝[次数,None]
+        headerTable[k] = [headerTable[k], None]  # 构造 reformat headerTable to use Node link
     # print 'headerTable: ',headerTable
+
     retTree = treeNode('Null Set', 1, None)  # create tree
-    for tranSet, count in dataSet.items():  # go through dataset 2nd time
-        localD = {}
-        for item in tranSet:  # put transaction items in order
-            if item in freqItemSet:
-                localD[item] = headerTable[item][0]
+    for tranSet, count in dataSet.items():  # 所有子集合。go through dataset 2nd time
+        localD = {} #保存一个子集合内的次数多的单词
+        for item in tranSet:  # tranSet子集合内的所有单词   put transaction items in order
+            if item in freqItemSet: #在整个集合的所有单词中，出现次数多的单词
+                localD[item] = headerTable[item][0] #key=单词，value=个数
         if len(localD) > 0:
+            # 子集合内的高频单词按次数排序
             orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
             updateTree(orderedItems, retTree, headerTable, count)  # populate tree with ordered freq itemset
     return retTree, headerTable  # return tree and header table
 
 
-def updateTree(items, inTree, headerTable, count):
-    if items[0] in inTree.children:  # check if orderedItems[0] in retTree.children
-        inTree.children[items[0]].inc(count)  # incrament count
+#headerTable是字典：key=单词，value＝[次数,None]
+def updateTree(items, inTree, headerTable, count):#将子集合内的高频单词，形成树:一个子集合内的高频单词，形成父节点－》子节点－》子节点
+    key=items[0] #高频单词
+    if key in inTree.children:  # check if orderedItems[0] in retTree.children
+        inTree.children[key].inc(count)  # incrament count
     else:  # add items[0] to inTree.children
-        inTree.children[items[0]] = treeNode(items[0], count, inTree)
-        if headerTable[items[0]][1] == None:  # update header table
-            headerTable[items[0]][1] = inTree.children[items[0]]
+        inTree.children[key] = treeNode(key, count, inTree)
+        if headerTable[key][1] == None:  # update header table
+            headerTable[key][1] = inTree.children[key]
         else:
-            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+            updateHeader(headerTable[key][1], inTree.children[key])
     if len(items) > 1:  # call updateTree() with remaining ordered items
-        updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+        #剩下的单词，继续生成树，作为items［0］的子节点
+        updateTree(items[1::], inTree.children[key], headerTable, count)
 
 
 def updateHeader(nodeToTest, targetNode):  # this version does not use recursion
@@ -77,19 +83,31 @@ def createInitSet(dataSet):
         retDict[frozenset(trans)] = 1
     return retDict
 
-# rootNode=treeNode('pyamid',9,None)
-# rootNode.children['eye']=treeNode('eye',13,None)
+# rootNode=treeNode('root',9,None)
+# eye=treeNode('eye',12,rootNode)
+# eye1=treeNode('eye1',13,eye)
+# eye2=treeNode('eye2',14,eye)
+# eye3=treeNode('eye3',16,eye)
+
+# eye2_1=treeNode('eye2_1',15,eye2)
+# eye2_2=treeNode('eye2_2',15,eye2)
+
+# eye2.children['eye2_1']=eye2_1
+# eye2.children['eye2_2']=eye2_2
+# eye.children['e1']=eye1
+# eye.children['e2']=eye2
+# eye.children['e3']=eye3
+# rootNode.children['eye']=eye
+#
 # rootNode.disp()
+
+# cc=rootNode.children
+# print(cc)
 
 simpData=loadSimpDat()
 initSet=createInitSet(simpData)
 mytree,headerTab=createTree(initSet,3)
-# mytree.disp()
-
-
-
-
-
+mytree.disp()
 
 
 

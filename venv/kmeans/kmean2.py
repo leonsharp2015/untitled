b2470681ddfea673c38e682c9036ba0d48bd10ae
leonsharp2015 <leonsharp2015@sina.com>
2020-02-05 01:17:47 +0800

    kmean2

--- venv/kmeans/kmean2.py
+++ venv/kmeans/kmean2.py
@@ -24,33 +24,37 @@ def kmeans2(dataSet,k):
     m=dataSet.shape[0]
     dataAssment=mat(zeros((m,2)))
     centids=randCent(dataSet,k)
-    for i in range(m):
-        v1=dataSet[i,:]
-        min_distance=inf
-        centid_index=0
-        for j in range(k):
-            c=centids[j,:]
-            dis=disEclud(v1,c)
-            if(dis<min_distance):
-                min_distance=dis
-                centid_index=j
-        dataAssment[i,:]=[min_distance,centid_index]
+    loop=True
+    while(loop):
+        loop=False
+        for i in range(m):
+            v1=dataSet[i,:]
+            min_distance=inf
+            centid_index=-1
+            for j in range(k):
+                c=centids[j,:]
+                dis=disEclud(v1,c)
+                if(dis<min_distance):
+                    min_distance=dis
+                    centid_index=j
+            if dataAssment[i, 1:2]!=centid_index:
+                loop=True
+            dataAssment[i,:]=[min_distance,centid_index]
 
-    # #0类
-    # rows_index_0=nonzero(dataAssment[:,1:2]==0)[0]
-    # data_0=dataSet[rows_index_0,:]
-    # new_cent_x_0=sum(data_0[:,:1])/data_0.shape[0]
-    # new_cent_y_0 = sum(data_0[:, 1:2]) / data_0.shape[0]
-    # #1类
-    # rows_index_1=nonzero(dataAssment[:,1:2]==1)[0]
-    # data_1=dataSet[rows_index_1,:]
-    # new_cent_x_1=sum(data_1[:,:1])/data_0.shape[0]
-    # new_cent_y_1 = sum(data_1[:, 1:2]) / data_0.shape[0]
+        # #0类
+        # rows_index_0=nonzero(dataAssment[:,1:2]==0)[0]
+        # data_0=dataSet[rows_index_0,:]
+        # new_cent_x_0=sum(data_0[:,:1])/data_0.shape[0]
+        # new_cent_y_0 = sum(data_0[:, 1:2]) / data_0.shape[0]
+        # #1类
+        # rows_index_1=nonzero(dataAssment[:,1:2]==1)[0]
+        # data_1=dataSet[rows_index_1,:]
+        # new_cent_x_1=sum(data_1[:,:1])/data_0.shape[0]
+        # new_cent_y_1 = sum(data_1[:, 1:2]) / data_0.shape[0]
 
-    #通过dataAssment第二列保存的row_index,分类data.取分组data的列均值作为点坐标
-    for i in range(k):
-        data=dataSet[nonzero(dataAssment[:,1:2]==i)[0],:]
-        centids[i,:]=mean(data,axis=0)
+        for i in range(k):#通过dataAssment第二列保存的row_index,分类data.取分组data的列均值作为点坐标
+            data=dataSet[nonzero(dataAssment[:,1:2]==i)[0],:]
+            centids[i,:]=mean(data,axis=0)
 
     return dataAssment
 
